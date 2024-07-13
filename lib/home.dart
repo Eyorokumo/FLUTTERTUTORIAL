927f00cadc2ffe9222a23b265dbdfc4b4043ece4
Eyorokumo <eyorokorumodicksonemmanuel@gmail.com>
2024-07-13 18:13:52 +0100

    fluttercourse

---
+++ lib/home.dart
@@ -0,0 +1,279 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text(
+          'Your i no know Monei Online Store',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+      body: SingleChildScrollView(child: Column(children: [
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+
+        //
+
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: const Text('Start from somewhere'),
+          leading: const Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+
+//
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+
+        //
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+
+        //ListTile(
+
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+//
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+//
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+//
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+//
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+
+//
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+
+//
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+
+//
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+
+//
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+
+//
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+//
+        ListTile(
+          title: Text('Momo Task'),
+          subtitle: Text('Start from somewhere'),
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.amber,
+            size: 45,
+          ),
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          onTap: () {
+            print('Elon Smart');
+          },
+        ),
+      ])),
+      // body: Center(child: Text('My First Flutter Application one,Hi Dear Welcome to Our crossplatform Code base site',style: TextStyle(color:Colors.orange,fontWeight:FontWeight.bold,fontSize:25.0,fontStyle:FontStyle.italic,),)),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.message),
+        backgroundColor: Colors.amber,
+        onPressed: () {
+          print("Hi Dear Welcome to Our crossplatform Code base site");
+        },
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import './home.dart';
+import './contact.dart';
+
+void main() {
+  var fontWeight;
+  runApp(MaterialApp(title: 'My First App', home: HomeScreen()));
+}
